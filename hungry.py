28c0889768915b133b43a830cec476e550de5860
TANISHK520 <kapoortanish74@gmail.com>
2023-01-05 11:29:43 +0530

    pixxa

--- hungry.py
+++ hungry.py
@@ -2,6 +2,8 @@
 hungry=input("are you hungry")
 if hungry=="yes":
     print("eat samosa")
+    print("eat paties")
+    print("tamoa")
 else:
     thirsty=input("are you thirsty")
     if thirsty=="yes":
