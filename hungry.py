da331bdadb5f0b702dbab3e2bc78f3dce0b36adf
TANISHK520 <kapoortanish74@gmail.com>
2023-01-05 10:32:29 +0530

    water

--- hungry.py
+++ hungry.py
@@ -3,4 +3,9 @@ hungry=input("are you hungry")
 if hungry=="yes":
     print("eat samosa")
 else:
+    thirsty=input("are you thirsty")
+    if thirsty=="yes":
+        print("hello")
+    else:
+        print("bye")
     print("do your homework")
